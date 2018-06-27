9b3f5b2845d3b6e1072c6408a36d137bf1bb1a33
Zomebody <guido.de.vries@ziggo.nl>
2018-06-27 23:14:49 +0200

    Algorithm modes, new colors

--- balanceteams.js
+++ balanceteams.js
@@ -2,6 +2,9 @@
 // Timer variables, cut-off when difference > 5000ms
 var t0 = 0;//performance.now();
 var t1 = 0;//performance.now();
+var mustPlayArray = null;
+var checkTeam = null;
+var optCheck = null;
 
 // Order in which roles will be filled into the teams
 var slot_order = [
@@ -87,39 +90,39 @@ function getSlotRoles(plrs) {
 // Checks if the current team is better, then replaces it if so
 var bestTeam = null;
 var lowestDif = Infinity;
-function checkBestTeam(comb) {
-	/*
-	totalBlueTSF = 0;
-	totalRedTSF = 0;
-	totalBlueDPS = 0;
-	totalRedDPS = 0;
+var mostUnique = 0;//Infinity; //mustPlayArray (arrA.filter(function(e) {return !arrB.includes(e);})).length
+var mustPick = true;
+
+// Function used when there are must-picks
+function checkBestMustTeam(comb) {
+	var totalBlue = 0;
+	var totalRed = 0;
 	for (g = 0; g < 12; g++) {
 		if (g < 6) { // blue player
-			if (comb[g]['role'] != 'DPS') {
-				totalBlueTSF += comb[g]['sr'];
-			} else {
-				totalBlueDPS += comb[g]['sr'];
-			}
+			totalBlue += comb[g]['sr'];
 		} else { // red player
-			if (comb[g]['role'] != 'DPS') {
-				totalRedTSF += comb[g]['sr'];
-			} else {
-				totalRedDPS += comb[g]['sr'];
-			}
+			totalRed += comb[g]['sr'];
 		}
 	}
-	avgBlue = (totalBlueTSF + totalBlueDPS)/6;
-	avgRed = (totalRedTSF + totalRedDPS)/6;
-	if (Math.abs(totalBlueTSF-totalRedTSF) + Math.abs(totalBlueDPS-totalRedDPS) < lowestDif) {
+	var avgBlue = totalBlue/6;
+	var avgRed = totalRed/6;
+	var filtered = comb.filter(function(plr) {return mustPlayArray.includes(plr['name']);});  // filter might be a bottleneck!
+	//if (filtered.length > mostUnique || Math.abs(totalBlue-totalRed) < lowestDif) {
+	if (filtered.length >= mostUnique && Math.abs(totalBlue-totalRed) < lowestDif) {
 		bestTeam = {
 			blue: comb.slice(0, 6),
 			red: comb.slice(6, 12),
 			blueAvg: avgBlue,
 			redAvg: avgRed
 		}
-		lowestDif = Math.abs(totalBlueTSF-totalRedTSF) + Math.abs(totalBlueDPS-totalRedDPS);
+		lowestDif = Math.abs(totalBlue-totalRed);
+		mostUnique = filtered.length;
 	}
-	*/
+}
+
+// Function used when there are no must-picks
+function checkBestTeam(comb) {
+	//var t_start = performance.now();
 	var totalBlue = 0;
 	var totalRed = 0;
 	for (g = 0; g < 12; g++) {
@@ -140,7 +143,26 @@ function checkBestTeam(comb) {
 		}
 		lowestDif = Math.abs(totalBlue-totalRed);
 	}
+	//t_filter += (performance.now()-t_start);
 }
+
+
+function quickContinueCheck() {
+	if (mustPick) {
+		if (mostUnique < Math.min(mustPlayArray.length, 12)) {
+			return true;
+		} else {
+			return lowestDif > 10;
+		}
+	} else {
+		return lowestDif > 2;
+	}
+}
+
+function continueCheck() {
+	return true;
+}
+
 
 // Finds and stores all combinations (overwrites best stored combination)
 var curCombination = [null, null, null, null, null, null, null, null, null, null, null, null];
@@ -158,23 +180,21 @@ function findCombinations(slotInfo, teamroles, tProgress, depth) {
 		// range includes numbers up to plrs.length-1 excluding those also in the blueindexes array
 		var indexes = arrayRange(roleInfo['prev']+1, plrs.length);
 		if (curTeam == 'red') { // IN THE PYTHON CODE THIS IS WHERE I MADE A MISTAKE, I PUT BLUE INSTEAD OF RED
-			indexes = (arrayRange(roleInfo['prev']+1, plrs.length)).filter(function(e) {return !slotInfo[curRole]['blueindexes'].includes(e);});
-			//console.log(indexes);
-		} //else {console.log(indexes, roleInfo['prev']+1, plrs.length);}
+			indexes = (arrayRange(roleInfo['prev']+1, plrs.length)).filter(function(e) {return !slotInfo[curRole]['blueindexes'].includes(e);}); // filter might be a bottleneck
+		}
 		
 		indexes.forEach(function(num) {
-			//console.log("slot:", roleInfo['slots'][roleInfo['fill']]);
 			curCombination[roleInfo['slots'][roleInfo['fill']]-1+(curTeam=='red' ? 6 : 0)] = plrs[num]; // no clue why the fuck I had to put -1 in here (my offset was wrong for some reason)
 			if (curTeam == 'blue') { // push to blueindexes
 				slotInfo[curRole]['blueindexes'].push(num);
 			}
 			roleInfo['fill']++;
 			if (depth == 11) {
-				checkBestTeam(curCombination);
+				checkTeam(curCombination);
 			} else {
 				var oldPrev = roleInfo['prev'];
 				roleInfo['prev'] = num;
-				if (performance.now() - t0 < 10000) { // do not enter recursion when you're running low on time!
+				if (performance.now() - t0 < 10000 && optCheck()) { // do not enter recursion when you're running low on time!
 					findCombinations(slotInfo, teamroles, tProgress, depth+1);
 				}
 				roleInfo['prev'] = oldPrev;
@@ -186,6 +206,8 @@ function findCombinations(slotInfo, teamroles, tProgress, depth) {
 		});
 	}
 }
+
+//(arrA.filter(function(e) {return !arrB.includes(e);})).length;
 
 /* arrA minus arrB
 arrA = [1, 3, 4, 7];
@@ -247,8 +269,13 @@ function startCombinations(sRoles, plrs) {
 }
 
 // Fairly balances teams and returns teams
-function generateTeams(plrsArray) { // player example: {name: 'John', sr: 5000, role: 'Flex'}
+function generateTeams(plrsArray, mustPlayNameArray, mustPicks, optimize) { // player example: {name: 'John', sr: 5000, role: 'Flex'}
 	// Step 1. Return value in advance + reset global variables
+	console.log("mustPicks:", mustPicks, "optimize:", optimize);
+	checkTeam = (mustPicks == true) ? checkBestMustTeam : checkBestTeam;
+	optCheck = (optimize == true) ? quickContinueCheck : continueCheck;
+	mustPlayArray = mustPlayNameArray;
+	mustPick = mustPicks;
 	var generated = {
 		teams: {
 			blue: [],
@@ -264,6 +291,7 @@ function generateTeams(plrsArray) { // player example: {name: 'John', sr: 5000, 
 	curCombination = [null, null, null, null, null, null, null, null, null, null, null, null];
 	lowestDif = Infinity;
 	bestTeam = null;
+	mostUnique = 0; //Infinity
 	// Step 2. Set slot roles
 	slotRoles = getSlotRoles(plrsArray); // returns {blue: [...], red: [...]}
 	if (typeof(slotRoles) == 'string') {
@@ -285,7 +313,7 @@ function generateTeams(plrsArray) { // player example: {name: 'John', sr: 5000, 
 	}
 	t1 = performance.now();
 	console.log("Team generation took " + (t1 - t0) + " milliseconds for " + plrsArray.length + " players.");
-	if (t1 - t0 >= 1000) {
+	if (t1 - t0 >= 10000) {
 		generated['errorMessage'] = "10 second timeout reached.";
 	}
 	return generated;
